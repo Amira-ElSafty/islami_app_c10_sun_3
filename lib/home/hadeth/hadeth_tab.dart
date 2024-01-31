15aa2a82d300e390f0880c437d079624a5bf5434
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2024-01-31 19:27:13 +0200

    finished quran tab. finished hadeth tab.

--- lib/home/hadeth/hadeth_tab.dart
+++ lib/home/hadeth/hadeth_tab.dart
@@ -1,10 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app_islami_c10_sun3/home/hadeth/item_hadeth_name.dart';
+import 'package:flutter_app_islami_c10_sun3/my_theme.dart';
 
-class HadethTab extends StatelessWidget {
+class HadethTab extends StatefulWidget {
+  @override
+  State<HadethTab> createState() => _HadethTabState();
+}
+
+class _HadethTabState extends State<HadethTab> {
+  List<Hadeth> hadethList = [];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pink,
+    if (hadethList.isEmpty) {
+      loadFile();
+    }
+    return Column(
+      children: [
+        Center(child: Image.asset('assets/images/hadeth_logo.png')),
+        Divider(
+          color: MyTheme.primaryLightColor,
+          thickness: 3,
+        ),
+        Text(
+          'Hadeth Name',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        Divider(
+          color: MyTheme.primaryLightColor,
+          thickness: 3,
+        ),
+        hadethList.isEmpty
+            ? Center(
+                child: CircularProgressIndicator(
+                  color: MyTheme.primaryLightColor,
+                ),
+              )
+            : Expanded(
+                child: ListView.separated(
+                  separatorBuilder: (context, index) {
+                    return Divider(
+                      color: MyTheme.primaryLightColor,
+                      thickness: 2,
+                    );
+                  },
+                  itemBuilder: (context, index) {
+                    return ItemHadethName(
+                      hadeth: hadethList[index],
+                    );
+                  },
+                  itemCount: hadethList.length,
+                ),
+              )
+      ],
     );
   }
+
+  void loadFile() async {
+    String hadethContent =
+        await rootBundle.loadString('assets/files/ahadeth.txt');
+    List<String> ahadethList = hadethContent.split('#\r\n');
+    for (int i = 0; i < ahadethList.length; i++) {
+      print(ahadethList[i]);
+      List<String> hadethLines = ahadethList[i].split('\n');
+      String title = hadethLines[0];
+      hadethLines.removeAt(0);
+      Hadeth hadeth = Hadeth(title: title, content: hadethLines);
+      hadethList.add(hadeth);
+      setState(() {});
+    }
+  }
+}
+
+class Hadeth {
+  String title;
+
+  List<String> content;
+
+  Hadeth({required this.title, required this.content});
 }
--- lib/my_theme.dart
+++ lib/my_theme.dart
@@ -4,15 +4,25 @@ class MyTheme {
   /// colors , light mode , dark mode
   static Color blackColor = Color(0xff242424);
   static Color primaryLightColor = Color(0xffB7935F);
+  static Color whiteColor = Color(0xffffffff);
   static ThemeData lightMode = ThemeData(
-      primaryColor: primaryLightColor,
-      scaffoldBackgroundColor: Colors.transparent,
-      appBarTheme: AppBarTheme(
-          backgroundColor: Colors.transparent, elevation: 0, centerTitle: true),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        selectedItemColor: blackColor,
-      ),
-      textTheme: TextTheme(
-          titleLarge: TextStyle(
-              fontSize: 30, fontWeight: FontWeight.bold, color: blackColor)));
+    primaryColor: primaryLightColor,
+    scaffoldBackgroundColor: Colors.transparent,
+    appBarTheme: AppBarTheme(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(color: blackColor),
+        centerTitle: true),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: blackColor,
+    ),
+    textTheme: TextTheme(
+      titleLarge: TextStyle(
+          fontSize: 30, fontWeight: FontWeight.bold, color: blackColor),
+      titleMedium: TextStyle(
+          fontSize: 25, fontWeight: FontWeight.w600, color: blackColor),
+      titleSmall: TextStyle(
+          fontSize: 25, fontWeight: FontWeight.w400, color: blackColor),
+    ),
+  );
 }
